523a6d680c97afa00c8b8970cb1beb11ff6b43ad
Alcides <guussaarloos@outlook.com>
2022-09-27 13:32:28 +0200

    Basics for server

--- ServerSolution/Server/Program.cs
+++ ServerSolution/Server/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Server server = new(1337);
+            server.Run();
         }
     }
 }
---
+++ ServerSolution/Server/Server.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Server
+    {
+        private static Socket serverSocket;
+        private static List<Client> clientList = new List<Client>();
+
+        private int _port;
+        struct Client
+        {
+            public Socket Socket { get; }
+            public int Id { get; }
+            public Client(Socket socket, int id)
+            {
+                Socket = socket;
+                Id = id;
+            }
+        }
+
+        public Server(int port)
+        {
+            _port = port;
+
+            // Creating an endpoint and defining the protocol used for communicating.
+            serverSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endPoint = new(IPAddress.Parse("127.0.0.1"), _port);
+
+            // running server
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(100);
+
+            Console.WriteLine($"Server setup and running on {endPoint.Address}:{endPoint.Port}");
+        }
+
+        public void Run()
+        {
+            // new thread to listen to incoming connections.
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    // New incoming connection.
+                    Socket socket = serverSocket.Accept();
+
+                    // saving client to list.
+                    Client client = new(socket, clientList.Count + 1);
+                    clientList.Add(client);
+
+                    // Every client gets its own thread.
+                    new Thread(() =>
+                    {
+                        HandleClient(client);
+                    }).Start();
+                }
+            }).Start();
+        }
+
+        private void HandleClient(Client client)
+        {
+            Console.WriteLine($"Client connection from: {client.Socket.RemoteEndPoint}");
+            byte[] message = new byte[1024];
+
+            // While the client is connected.
+            while (client.Socket.Connected)
+            {
+                try
+                {
+                    int receive = client.Socket.Receive(message);
+                    string data = Encoding.ASCII.GetString(message, 0, receive);
+                    Console.WriteLine($"Message from: {client.Id}: {data}");
+                    Thread.Sleep(100);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"ERROR WITH CLIENT: {e}"); 
+                }
+            }
+        }
+    }
+}

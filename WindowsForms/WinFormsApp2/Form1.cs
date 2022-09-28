2313e60cb0539169bc3d6019ad1a2af1d335046a
koen <kvoostrom011@gmail.com>
2022-09-28 13:49:30 +0200

    fixed simple windows forms

---
+++ WindowsForms/WinFormsApp2/Form1.cs
@@ -0,0 +1,37 @@
+namespace WinFormsApp2
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox5.Text = "hallo Niels";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (i % 2 == 0)
+            {
+                textBox4.Text = "Guus";
+                i++;
+            }
+            else
+            {
+                textBox4.Text = "Niet Niels";
+                i++;
+            }
+
+        }
+    }
+}

2bb64e2593b78ce967f70295fbe88284657ba5d3
koen <kvoostrom011@gmail.com>
2022-09-29 13:15:57 +0200

    added windows forms for screens needed

--- GUI/AccountTypeSelector.cs
+++ GUI/AccountTypeSelector.cs
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace RemoteHealthCare.GUI
 {
-    public partial class Form1 : Form
+    public partial class AccountTypeSelector : Form
     {
-        public Form1()
+        public AccountTypeSelector()
         {
             InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
---
+++ GUI/ClientScreen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoteHealthCare.GUI
+{
+    public partial class ClientScreen : Form
+    {
+        public ClientScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}

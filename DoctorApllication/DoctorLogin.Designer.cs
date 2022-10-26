0c4908201e3b0f64762053c56302900e23e5e9d4
koen <kvoostrom011@gmail.com>
2022-10-26 23:01:00 +0200

    design login screen

--- DoctorApllication/DoctorLogin.Designer.cs
+++ DoctorApllication/DoctorLogin.Designer.cs
@@ -38,26 +38,31 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.txtLoginInfo = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(84, 257);
+            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(65, 228);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(84, 300);
+            this.button2.BackColor = System.Drawing.Color.PaleGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(65, 277);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(88, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "New Account";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // contextMenuStrip1
@@ -67,21 +72,28 @@
             // 
             // txtAccountName
             // 
+            this.txtAccountName.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtAccountName.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtAccountName.Location = new System.Drawing.Point(34, 88);
             this.txtAccountName.Name = "txtAccountName";
-            this.txtAccountName.Size = new System.Drawing.Size(154, 23);
+            this.txtAccountName.PlaceholderText = "Account name";
+            this.txtAccountName.Size = new System.Drawing.Size(154, 16);
             this.txtAccountName.TabIndex = 0;
             // 
             // txtPassword
             // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtPassword.Location = new System.Drawing.Point(34, 167);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(154, 23);
+            this.txtPassword.PlaceholderText = "Password";
+            this.txtPassword.Size = new System.Drawing.Size(154, 16);
             this.txtPassword.TabIndex = 1;
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.Color.PapayaWhip;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Location = new System.Drawing.Point(34, 145);
             this.textBox1.Name = "textBox1";
@@ -92,6 +104,7 @@
             // 
             // textBox2
             // 
+            this.textBox2.BackColor = System.Drawing.Color.PapayaWhip;
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Location = new System.Drawing.Point(34, 66);
             this.textBox2.Name = "textBox2";
@@ -102,6 +115,7 @@
             // 
             // txtLoginInfo
             // 
+            this.txtLoginInfo.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.txtLoginInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.txtLoginInfo.Location = new System.Drawing.Point(34, 12);
             this.txtLoginInfo.Name = "txtLoginInfo";
@@ -109,11 +123,24 @@
             this.txtLoginInfo.Size = new System.Drawing.Size(154, 16);
             this.txtLoginInfo.TabIndex = 6;
             // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.PapayaWhip;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(109, 343);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(124, 15);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "Doctor login V1.0";
+            // 
             // DoctorLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(240, 413);
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(228, 358);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.txtLoginInfo);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -140,5 +167,6 @@
         private TextBox textBox2;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private TextBox txtLoginInfo;
+        private TextBox textBox3;
     }
 }
